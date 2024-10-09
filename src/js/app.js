06186a0b63ab7722c302e1e0b2158e93db889f9d
FabFu19 <fabianfuen19@icloud.com>
2024-10-09 18:23:33 +0000

    feat: properties added in render function

--- src/js/app.js
+++ src/js/app.js
@@ -33,14 +33,38 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${variables.name ? variables.name : "Orlando"} ${
+    variables.lastName ? variables.lastNameame : "Diaz"
+  }</h1>
+          <h2>${variables.role ? variables.role : "Engineer"}</h2>
+          <h3>${variables.city ? variables.city : "Buenos Aires"}, ${
+    variables.country ? variables.country : "AR"
+  }</h3>
+          <ul class="${
+            variables.socialMediaPosition
+              ? variables.socialMediaPosition
+              : "Position right"
+          }">
+            <li><a href="${
+              variables.twitter
+                ? variables.twitter
+                : "https://x.com/4geeksacademy?s=21&t=1KtvCQsso_w9I3rQhVuyog"
+            }" target="_blank"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="${
+              variables.github
+                ? variables.github
+                : "https://github.com/4GeeksAcademy"
+            }" target="_blank"><i class="fab fa-github"></i></a></li>
+            <li><a href="${
+              variables.linkedin
+                ? variables.linkedin
+                : "https://github.com/4GeeksAcademy"
+            }" target="_blank"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="${
+              variables.instagram
+                ? variables.instagram
+                : "https://github.com/4GeeksAcademy"
+            }" target="_blank"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
